60c469e2f4797c03baf008e90811df1183d1e3cb
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-03-20 06:23:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,5 @@
 def speak_to_grandma(phrase1)
+<<<<<<< HEAD
   if phrase1 == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase1 == phrase1.upcase
@@ -6,4 +7,7 @@ return "NO, NOT SINCE 1938!"
 elsif phrase1 != phrase1.upcase
   return "HUH?! SPEAK UP, SONNY!"
 end
+=======
+
+>>>>>>> 5066bc783892f2937a61d879c391c3c09877344b
 end
